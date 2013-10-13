491c9014bafe543a574439fa2f31b7dfcbd5e2fb
Bagdat <bbb1991@mail.ru>
2013-10-14 03:56:52 +0600

    Create FibonacciNumber.java

---
+++ FibonacciNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class FibonacciNumber {
+
+    static int fibonacci(int n) {
+        return (n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2));
+    }
+
+
+    public static void main(String[] args)  {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество цифр, которую нужно вывести:");
+
+
+        try {
+            int max = scanner.nextInt();
+
+            for (int n=1; n<=max; n++) {
+                System.out.print(fibonacci(n)+", ");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка! Введите целое число!");
+        }
+    }
+}
